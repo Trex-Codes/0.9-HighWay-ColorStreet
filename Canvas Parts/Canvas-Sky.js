00fb7f7817e6985f379dcff0a98620501e455ad6
Trex_Codes <30562611+juan1305@users.noreply.github.com>
2020-09-06 01:56:53 -0500

    6.0 ADD file Sky.js

---
+++ Canvas Parts/Canvas-Sky.js
@@ -0,0 +1,4 @@
+document.getElementById('Change_Object_Sky').onclick = function() {
+	let colorasksun1 = prompt('enter the color of the sky');
+	document.getElementById('CanvasHtml').style.backgroundColor = colorasksun1;
+}
