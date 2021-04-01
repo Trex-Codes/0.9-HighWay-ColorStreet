cb1f28dc67d2ec6a2dd5c22103189dcf1501f71a
Trex-Codes <juan.13JSG@gmail.com>
2021-04-01 07:33:58 -0500

    9.0 CHANGE AND FIX files (code) refactoring (HTML-CSS-JS)

--- Canvas Parts/Canvas-Mountains.js
+++ Canvas Parts/Canvas-Mountains.js
@@ -1,106 +1,104 @@
 // (1)
-	ctx.beginPath();
-	ctx.fillStyle = 'green';
-	ctx.strokeStyle = "black";
-	ctx.moveTo(0, 400);
-	ctx.quadraticCurveTo(158, -50, 317, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.strokeStyle = "black";
+ctx.moveTo(0, 400);
+ctx.quadraticCurveTo(158, -50, 317, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (2)
-	ctx.beginPath();
-	ctx.fillStyle = 'green';
-	ctx.strokeStyle = "black";
-	ctx.moveTo(317, 400);
-	ctx.quadraticCurveTo(474, -50, 634, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (2)
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.strokeStyle = "black";
+ctx.moveTo(317, 400);
+ctx.quadraticCurveTo(474, -50, 634, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (3)
-	ctx.beginPath();
-	ctx.fillStyle = 'green';
-	ctx.strokeStyle = "black";
-	ctx.moveTo(634, 400);
-	ctx.quadraticCurveTo(790, -50, 951, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (3)
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.strokeStyle = "black";
+ctx.moveTo(634, 400);
+ctx.quadraticCurveTo(790, -50, 951, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (4)
-	ctx.beginPath();
-	ctx.fillStyle = 'green';
-	ctx.strokeStyle = "black";
-	ctx.moveTo(951, 400);
-	ctx.quadraticCurveTo(1106, -50, 1268, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (4)
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.strokeStyle = "black";
+ctx.moveTo(951, 400);
+ctx.quadraticCurveTo(1106, -50, 1268, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (5)
-	ctx.beginPath();
-	ctx.fillStyle = 'green';
-	ctx.strokeStyle = "black";
-	ctx.moveTo(1268, 400);
-	ctx.quadraticCurveTo(1422, -50, 1585, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (5)
+ctx.beginPath();
+ctx.fillStyle = 'green';
+ctx.strokeStyle = "black";
+ctx.moveTo(1268, 400);
+ctx.quadraticCurveTo(1422, -50, 1585, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-// -----------------------------------------------
+// FIGURE MOUNTAINS
+document.getElementById('Change_Object_Moun').onclick = function() {
+let colorasksun1 = prompt('Enter the color of the mountains');
 
-	// FIGURE MOUNTAINS
-	document.getElementById('Change_Object_Moun').onclick = function() {
-	let colorasksun1 = prompt('Enter the color of the mountains');
-	
-	// (1)
-	ctx.beginPath();
-	ctx.fillStyle = colorasksun1;
-	ctx.strokeStyle = "black";
-	ctx.moveTo(0, 400);
-	ctx.quadraticCurveTo(158, -50, 317, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (1)
+ctx.beginPath();
+ctx.fillStyle = colorasksun1;
+ctx.strokeStyle = "black";
+ctx.moveTo(0, 400);
+ctx.quadraticCurveTo(158, -50, 317, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (2)
-	ctx.beginPath();
-	ctx.fillStyle = colorasksun1;
-	ctx.strokeStyle = "black";
-	ctx.moveTo(317, 400);
-	ctx.quadraticCurveTo(474, -50, 634, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (2)
+ctx.beginPath();
+ctx.fillStyle = colorasksun1;
+ctx.strokeStyle = "black";
+ctx.moveTo(317, 400);
+ctx.quadraticCurveTo(474, -50, 634, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (3)
-	ctx.beginPath();
-	ctx.fillStyle = colorasksun1;
-	ctx.strokeStyle = "black";
-	ctx.moveTo(634, 400);
-	ctx.quadraticCurveTo(790, -50, 951, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (3)
+ctx.beginPath();
+ctx.fillStyle = colorasksun1;
+ctx.strokeStyle = "black";
+ctx.moveTo(634, 400);
+ctx.quadraticCurveTo(790, -50, 951, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (4)
-	ctx.beginPath();
-	ctx.fillStyle = colorasksun1;
-	ctx.strokeStyle = "black";
-	ctx.moveTo(951, 400);
-	ctx.quadraticCurveTo(1106, -50, 1268, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (4)
+ctx.beginPath();
+ctx.fillStyle = colorasksun1;
+ctx.strokeStyle = "black";
+ctx.moveTo(951, 400);
+ctx.quadraticCurveTo(1106, -50, 1268, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-	// (5)
-	ctx.beginPath();
-	ctx.fillStyle = colorasksun1;
-	ctx.strokeStyle = "black";
-	ctx.moveTo(1268, 400);
-	ctx.quadraticCurveTo(1422, -50, 1585, 400);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// (5)
+ctx.beginPath();
+ctx.fillStyle = colorasksun1;
+ctx.strokeStyle = "black";
+ctx.moveTo(1268, 400);
+ctx.quadraticCurveTo(1422, -50, 1585, 400);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 }
--- Canvas Parts/Canvas_Sun.js
+++ Canvas Parts/Canvas_Sun.js
@@ -1,15 +1,13 @@
-	// SUN
-	ctx.beginPath();
-	ctx.fillStyle = "yellow";
-	ctx.strokeStyle = "black";
-	ctx.arc(1300, 95, 70, 0, Math.PI/180*360);
-	ctx.stroke();
-	ctx.fill();
-	ctx.closePath();
+// SUN
+ctx.beginPath();
+ctx.fillStyle = "yellow";
+ctx.strokeStyle = "black";
+ctx.arc(1300, 95, 70, 0, Math.PI/180*360);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
 
-// -----------------------------------------------
-
-	document.getElementById('Change_Object_Sun').onclick = function() {
+document.getElementById('Change_Object_Sun').onclick = function() {
 	let colorasksun1 = prompt('Enter the color of the sun');
 
 	// SUN
